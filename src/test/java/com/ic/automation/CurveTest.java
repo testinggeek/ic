ebb97e62e31ea83fca928914d23afdb993145fdd
Anand Ramdeo <Anand.Ramdeo@moonpig.com>
2016-07-12 22:03:08 +0100

    adds test for valid phone number submission

--- src/test/java/com/ic/automation/CurveTest.java
+++ src/test/java/com/ic/automation/CurveTest.java
@@ -31,6 +31,18 @@ public class CurveTest {
         assertTrue("Page title does not match", "Curve - All your cards in one".equalsIgnoreCase(driver.getTitle()));
     }
 
+    @Test
+    public void ensureThatUserCanSubmitMobileNumberToGetAppDownloadLink(){
+
+        driver.get(baseUrl + "/joinnow/");
+        driver.findElement(By.id("field_38kar")).isDisplayed();
+        driver.findElement(By.id("field_38kar")).sendKeys("7768996680");
+        driver.findElement(By.className("frm_submit")).click();
+        assertTrue("Success message is not present",driver.findElement(By.className("frm_message")).getText().contains("We've sent you a link to download the app"));
+
+    }
+
+
     @After
     public void tearDown() throws Exception {
         driver.quit();
